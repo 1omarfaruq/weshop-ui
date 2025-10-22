f073469c661cdd5c8ae0b0faf43a0c5b42ad3c81
eftakharulislam491 <mdeftakharulislamkhan@gmail.com>
2025-10-22 15:32:42 +0600

    fix css the issues

--- cart/cart.js
+++ cart/cart.js
@@ -10,7 +10,3 @@ function toggleCollapse(contentId, arrowId) {
     arrow.classList.add("rotate-180");
   }
 }
-
-// Initialize the first section as expanded if needed, though 'expanded' class is already in HTML for Notes
-// To make "Discounts" initially collapsed (as per image), ensure it doesn't have 'expanded' class initially.
-// If you want "Notes" to be collapsed initially, remove 'expanded' from its div in HTML.
